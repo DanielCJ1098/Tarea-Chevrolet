0f2bc8eb0446a7a8305dd4399e6a28f1861ab1fe
Dell <Dell@DESKTOP-6LRU2BN>
2022-06-05 23:12:53 -0600

    Commit Semana 3

---
+++ src/main/java/com/TareaChevrolet/TareaChevroletApplication.java
@@ -0,0 +1,13 @@
+package com.TareaChevrolet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaChevroletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaChevroletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TareaChevrolet/TareaChevroletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TareaChevrolet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaChevroletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
